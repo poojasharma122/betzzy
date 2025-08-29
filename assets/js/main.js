b2691b353ac0585e5b0a7d751210ee275ac9a8b3
frontenddev <frontendev344@gmail.com>
2025-08-29 11:39:16 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// Toggle Menu Functionaliy Start
+$(document).ready(function () {
+    $(".menu-toggle").click(function () {
+      $("body").addClass("menuToggle");
+    });
+    $(".menu-close").click(function () {
+      $("body").removeClass("menuToggle");
+    });
+  });
+  // Toggle Menu Functionaliy End
+  
+  // Header Scroll JS Start
+    $(document).ready(function () {
+      $(window).scroll(function () {
+        var header = $("header");
+        header.toggleClass("fixed_header", $(window).scrollTop() > 0);
+      });
+    });
+    // Header Scroll JS End
+    
+  
+  // AOS JS Start
+  AOS.init({
+    duration: 1200,
+  });
+  // AOS JS End
